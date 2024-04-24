57bd5c3af55a9ebb262d036061e5ba68105c55c5
juanmaishere <135636931+juanmaishere@users.noreply.github.com>
2024-04-24 15:24:40 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,17 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
+      <section>
       <Title>KelSoftware</Title>
       <h1>KelSoftware</h1>
        <h2>Soluciones de software a la medida
             Desarollo de Web y Apps</h2>
+        </section>
+      <section>
       <p>
-        D
+        Dddddddddddddddd
       </p>
+      <section/>
       
     </main>
   );
